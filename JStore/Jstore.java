a7badb5a3fa5f0f7409977ba42d8c71880ae9ddd
ernawulandarii <47845201+ernawulandarii@users.noreply.github.com>
2019-03-05 10:25:01 +0700

    Modul 1

---
+++ JStore/Jstore.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Jstore here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Jstore 
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    public Jstore()
+    {
+    }
+}
+            
+
